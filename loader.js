0e3a77fc9254b1775389a602e6284cf7e61d2d9e
Clement Mondary <clement.mondary@gmail.com>
2024-11-25 18:37:55 +0100

    acceleration de l'animation

--- loader.js
+++ loader.js
@@ -1,16 +1,19 @@
 let h = 0;
 const loaderFill = document.querySelector('.loader-fullscreen-fill');
-const loaderInterval = setInterval(() => {
-    h += 5;
-    if (h > 100) h = 0;
-    loaderFill.style.height = h + '%';
-}, 1000);
 
-// Rediriger vers la page de création de post après un court délai
-setTimeout(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const title = urlParams.get('post_title');
-    const content = urlParams.get('content');
-    const postUrl = `https://mondary.design/wp-admin/post-new.php?post_title=${encodeURIComponent(title)}&content=${encodeURIComponent(content)}`;
-    window.location.href = postUrl;
-}, 3000); // Délai de 3 secondes avant la redirection
+// Démarrer l'animation de chargement
+const loaderInterval = setInterval(() => {
+    h += 5; // Augmentez la hauteur de 5% à chaque intervalle
+    if (h > 100) {
+        clearInterval(loaderInterval); // Arrêtez l'intervalle lorsque la hauteur atteint 100%
+        // Rediriger vers la page de création de post après un court délai
+        setTimeout(() => {
+            const urlParams = new URLSearchParams(window.location.search);
+            const title = urlParams.get('post_title');
+            const content = urlParams.get('content');
+            const postUrl = `https://mondary.design/wp-admin/post-new.php?post_title=${encodeURIComponent(title)}&content=${encodeURIComponent(content)}`;
+            window.location.href = postUrl;
+        }, 500); // Délai de 0,5 seconde avant la redirection
+    }
+    loaderFill.style.height = h + '%'; // Mettre à jour la hauteur du loader
+}, 100); // Intervalle de 100 ms pour une animation fluide
